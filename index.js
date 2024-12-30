55c43b9281bebb071149e7b5421b0c0593b627af
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-12-30 23:24:04 +0600

    DB setup and Controller Created alng with Routes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDb.js";
+import authRoutes from "./routes/route.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res) => {
-    res.send("runninf at port 3000 fhfhf");
-})
+app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
-    console.log("App is running at port 3000");
+app.listen(PORT, () => {
+    connectDB();
+    console.log("App is running at port:", PORT);
 });
+
+// opgpJHGSbSImzD0z 
+
+// mongodb+srv://mdtahmidchowdhury12:opgpJHGSbSImzD0z@cluster0.1yd73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
