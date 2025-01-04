62c2a963c58ac5f9d675a8517700658484eadb55
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2025-01-04 21:17:12 +0600

    Some Authentication Codes

--- mailtrap/emails.js
+++ mailtrap/emails.js
@@ -1,5 +1,5 @@
 import { transporter, sender } from './mailtrap.js'; // Import the transporter
-import { VERIFICATION_EMAIL_TEMPLATE } from './emailTemplates.js';
+import { VERIFICATION_EMAIL_TEMPLATE,PASSWORD_RESET_REQUEST_TEMPLATE } from './emailTemplates.js';
 
 export const sendVerificationEmail = async (email, verificationToken) => {
     console.log("Send Verification Email", email)
@@ -16,5 +16,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     } catch (error) {
         console.error("Error sending verification email:", error);
         throw new Error(`Error sending verification email: ${error}`);
+    }
+};
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+    console.log("Send Password Reset Email", email);
+
+    try {
+        const mailOptions = {
+            from: sender, // Sender address
+            to: email,    // Recipient address
+            subject: "Reset your password",
+            html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+        };
+
+        const response = await transporter.sendMail(mailOptions);
+        console.log("Password reset email sent successfully:", response.messageId);
+    } catch (error) {
+        console.error("Error sending password reset email:", error);
+        throw new Error(`Error sending password reset email: ${error.message}`);
     }
 };
--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup,login,logout } from "../controllers/authController.js";
+import { signup,login,logout,verifyEmail,forgotPassword } from "../controllers/authController.js";
 
 const router = express.Router();
 
@@ -7,6 +7,10 @@ router.post("/signup", signup);
 
 router.post("/login", login);
 
+router.post("/verify-email", verifyEmail);
+
 router.post("/logout", logout);
+
+router.post("/forgot-password", forgotPassword);
 
 export default router;
